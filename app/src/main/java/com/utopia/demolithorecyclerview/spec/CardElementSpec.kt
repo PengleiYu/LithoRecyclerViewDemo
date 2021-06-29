1160ced387cc00d54cf3b11b10f6ae9e6ec321f4
yupenglei <yupengniaosanshi@gmail.com>
2021-06-29 12:00:21 +0800

    组件拆分

--- app/src/main/java/com/utopia/demolithorecyclerview/spec/CardElementSpec.kt
+++ app/src/main/java/com/utopia/demolithorecyclerview/spec/CardElementSpec.kt
@@ -1,10 +1,13 @@
 package com.utopia.demolithorecyclerview.spec
 
 import android.graphics.Color
+import android.graphics.Rect
 import android.graphics.Typeface
 import android.text.TextUtils
+import android.view.View
 import android.widget.ImageView
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.facebook.litho.Column
 import com.facebook.litho.Component
 import com.facebook.litho.ComponentContext
@@ -21,6 +24,7 @@ import com.facebook.litho.widget.Text
 import com.facebook.yoga.YogaEdge.*
 import com.utopia.demolithorecyclerview.R
 import com.utopia.demolithorecyclerview.data.bean.FeedType
+import timber.log.Timber
 
 @LayoutSpec
 class CardElementSpec {
@@ -35,15 +39,25 @@ class CardElementSpec {
             @Prop desc: String,
             @Prop photos: List<Int>,
         ): Component {
-            val titleComp = Text.create(c, 0, R.style.TextAppearance_AppCompat_Title)
-                .marginDip(TOP, 16f)
-                .marginDip(BOTTOM, 8f)
-                .marginDip(HORIZONTAL, 8f)
-                .typeface(Typeface.DEFAULT_BOLD)
-                .textColor(Color.BLACK)
-                .text(title)
+            return Column.create(c)
+                .child(createTitleComp(c, title))
+                .child(createImagesComp(c, type, photos))
+                .child(createDescComp(c, desc))
                 .build()
-            val descComp = Text.create(c)
+        }
+
+        private fun createImagesComp(c: ComponentContext, type: FeedType, photos: List<Int>) =
+            if (type == FeedType.PHOTO_FEED)
+                CardHorizontalRecycler.create(c)
+                    .imageResList(photos)
+                    .build()
+            else
+                CardInnerImage.create(c)
+                    .imageRes(photos[0])
+                    .build()
+
+        private fun createDescComp(c: ComponentContext, desc: String) =
+            Text.create(c)
                 .text(desc)
                 .marginDip(VERTICAL, 16f)
                 .marginDip(HORIZONTAL, 8f)
@@ -53,27 +67,27 @@ class CardElementSpec {
                 .maxLines(4)
                 .build()
 
-            return Column.create(c)
-                .child(titleComp)
-                .child(
-                    if (type == FeedType.PHOTO_FEED)
-                        getRecyclerComp(c, photos)
-                    else getImageComp(c, photos[0])
-                )
-                .child(descComp)
+        private fun createTitleComp(c: ComponentContext, title: String) =
+            Text.create(c, 0, R.style.TextAppearance_AppCompat_Title)
+                .marginDip(TOP, 16f)
+                .marginDip(BOTTOM, 8f)
+                .marginDip(HORIZONTAL, 8f)
+                .typeface(Typeface.DEFAULT_BOLD)
+                .textColor(Color.BLACK)
+                .text(title)
                 .build()
-        }
+    }
+}
 
-        private fun getImageComp(c: ComponentContext, imageRes: Int): Image {
-            return Image.create(c)
-                .widthPercent(100f)
-                .heightDip(200f)
-                .scaleType(ImageView.ScaleType.CENTER_CROP)
-                .drawableRes(imageRes)
-                .build()
-        }
-
-        private fun getRecyclerComp(c: ComponentContext, imageResList: List<Int>): Component {
+@LayoutSpec
+class CardHorizontalRecyclerSpec {
+    companion object {
+        @JvmStatic
+        @OnCreateLayout
+        fun onCreateLayout(
+            c: ComponentContext,
+            @Prop imageResList: List<Int>,
+        ): Component {
             return RecyclerCollectionComponent.create(c)
                 .heightDip(200f)
                 .recyclerConfiguration(
@@ -81,10 +95,11 @@ class CardElementSpec {
                         .orientation(LinearLayoutManager.HORIZONTAL)
                         .build()
                 )
+                .itemDecoration(CardInnerImageDecoration())
                 .section(
                     DataDiffSection.create<Int>(SectionContext(c))
                         .data(imageResList)
-                        .renderEventHandler(CardElement.onRenderImage(c))
+                        .renderEventHandler(CardHorizontalRecycler.onRenderImage(c))
                 )
                 .build()
         }
@@ -93,8 +108,47 @@ class CardElementSpec {
         @OnEvent(RenderEvent::class)
         fun onRenderImage(c: ComponentContext, @FromEvent model: Int): RenderInfo {
             return ComponentRenderInfo.create()
-                .component(getImageComp(c, model))
+                .component(
+                    CardInnerImage.create(c)
+                        .imageRes(model)
+                )
                 .build()
         }
+    }
+}
+
+@LayoutSpec
+class CardInnerImageSpec {
+    companion object {
+        @JvmStatic
+        @OnCreateLayout
+        fun onCreateLayout(
+            c: ComponentContext,
+            @Prop imageRes: Int,
+        ): Component {
+            return Image.create(c)
+                .widthPercent(100f)
+                .heightDip(200f)
+                .scaleType(ImageView.ScaleType.CENTER_CROP)
+                .drawableRes(imageRes)
+                .build()
+        }
+    }
+}
+
+class CardInnerImageDecoration : RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        val layoutManager = parent.layoutManager ?: return
+        val childCount = layoutManager.itemCount
+        val position = layoutManager.getPosition(view)
+        val isLast = position == childCount - 1
+        val width = if (isLast) 0 else 40
+        outRect.set(0, 0, width, view.height)
+        Timber.d("getItemOffsets:view=${view.hashCode()},index=$position,idLast=$isLast,rect=$outRect,childCount=$childCount")
     }
 }
